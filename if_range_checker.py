86138190a75ec1fc43fda7da4fc93d84e0483aa5
jeflecias <jefleciasg@gmail.com>
2024-10-28 20:49:10 +0800

    initial comments

---
+++ if_range_checker.py
@@ -0,0 +1,4 @@
+# PLD
+# ask the user to input a number from 1-50, and keep asking
+# if the user's input is invalid, display how many numbers are in the following ranges
+# 1-10, 11-20, 21-30, 31-40, 41-50
